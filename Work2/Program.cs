a706084ce62ada54ff4613d877ceecf2d82b6c5e
Matney0 <mlmoller21@amarillocollege.com>
2025-01-27 11:25:10 -0600

    Updated Program.cs
    Ive actually somewhat lost my mind trying to commit changes to the github without any progress. Ive connected Visual studio and my Github, etc so for now this is the only accessible way of commiting changes to my file.

--- Work2/Program.cs
+++ Work2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Work2
 {
@@ -6,7 +6,42 @@ namespace Work2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Added a user imput as an example
+            int score =73;
+            {
+                //First if condition.
+                if (score >= 89.5)
+                {
+                    //Presents Letter grade A if equal of more than 89.5
+                    Console.WriteLine("Grade:A")
+                }
+                //Second condition or option
+                else if (score >=79.5)
+                {
+                    //Presents Letter grade B if equal of more than 79.5
+                    Console.WriteLine("Grade:B")
+                }
+                //Third condition
+                else if (score >=69.5)
+                {
+                    //Presents Letter grade C if equal of more than 69.5
+                    Console.WriteLine("Grade:C")
+                }
+                //Fourth condition
+                else if (score >=59.5)
+                {
+                    //Presents Letter grade D if equal of more than 59.5
+                    Console.WriteLine("Grade:D")
+                }
+                //Last condition doesnt need to be tested as all other options tested first
+                else
+                {
+                    //Presents Letter grade F if all other if, and else if conditions exhuasted
+                    Console.WriteLine("Grade:F")
+                }
+            }
+               
         }
     }
 }
+
